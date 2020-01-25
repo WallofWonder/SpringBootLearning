652d344ba188f90d4fe2b204583653e6ba7d4790
ZYF <m13110683916@163.com>
2020-01-25 16:49:46 +0800

    3-5 加载旧项目配置文件的两种方式 @PropertySource @ImportResources

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource(locations = {"classpath:beans.xml"})
 public class DemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/demo/model/yaml/Family.java
+++ src/main/java/com/example/demo/model/yaml/Family.java
@@ -1,8 +1,10 @@
 package com.example.demo.model.yaml;
 
+import com.example.demo.config.MixPropertySourceFactory;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
@@ -12,6 +14,7 @@ import javax.validation.constraints.NotEmpty;
 @Component
 @Validated
 @ConfigurationProperties(prefix = "family")
+@PropertySource(value = {"classpath:family.yml"}, factory = MixPropertySourceFactory.class) // 引用外部配置
 public class Family {
 
     //@Value("${family.family-name}")
