59676c1fb0cff143d05a68d38919c247407aefd2
ZYF <m13110683916@163.com>
2020-01-25 22:45:58 +0800

    3-6 profile不同环境使用不同配置

--- src/main/java/com/example/demo/service/ArticleRestService.java
+++ src/main/java/com/example/demo/service/ArticleRestService.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Slf4j
 @Service
 public class ArticleRestService {
-    public String saveArticle(@RequestBody Article article) {
+    public String saveArticle(Article article) {
         //因为使用了lombok的Slf4j注解，这里可以直接使用log变量打印日志
         log.info("saveArticle：{}", article);
         return "测试";
