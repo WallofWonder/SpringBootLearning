730a0fb779e9f216e70c5cf25a8bbe4bc52dfcaf
ZYF <m13110683916@163.com>
2020-02-06 11:59:11 +0800

    11.8.集群多节点应用session共享

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.example.demo.generator"})
 @EnableCaching
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 30 * 60 * 1000)
 public class DemoApplication {
 
     public static void main(String[] args) {
