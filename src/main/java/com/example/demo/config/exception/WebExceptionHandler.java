623f92cd9270d08508c7e995982942f76611e75f
ZYF <m13110683916@163.com>
2020-02-11 19:42:56 +0800

    13.2.服务端推送事件SSE
    (整合freemarker)

--- src/main/java/com/example/demo/config/exception/WebExceptionHandler.java
+++ src/main/java/com/example/demo/config/exception/WebExceptionHandler.java
@@ -3,6 +3,10 @@ package com.example.demo.config.exception;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class WebExceptionHandler {
@@ -24,5 +28,13 @@ public class WebExceptionHandler {
 
         //没有被程序员发现，并转换为CustomException的异常，都是其他异常或者未知异常.
         return AjaxResponse.error(new CustomException(CustomExceptionType.OTHER_ERROR, "未知异常"));
+    }
+
+    @ExceptionHandler(AsyncRequestTimeoutException.class)
+    @ResponseBody
+    public String handleAsyncRequestTimeoutException(AsyncRequestTimeoutException e) {
+        return SseEmitter.event().data("timeout!!").build().stream()
+                .map(d -> d.getData().toString())
+                .collect(Collectors.joining());
     }
 }
